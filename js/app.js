131e563ff8960809b8dafbdddfd0c0df09ae2398
AMMARALBESANI <Ammarmohammed018@gmail.com>
2023-04-04 16:54:11 +0300

    fixing my fault2

--- js/app.js
+++ js/app.js
@@ -11,23 +11,23 @@ if (n == ""){
     let age = prompt("please"+" "+n+" "+"enter your age")
     if (age <=0){
         prompt("please enter valid age")
-    }else {
         let g = prompt("please"+" " +n+" "+"enter your gender")
-         if(g==="male"){
-           let mc = confirm("do you want to skip welcome")
-           if(mc === false){
-            alert("welcome Mr"+" "+n)
+        if(g==="male"){
+          let mc = confirm("do you want to skip welcome")
+          if(mc === false){
+           alert("welcome Mr"+" "+n)
+          }
+        }else if (g==="female"){
+           let fc = confirm("do you want to skip welcome")
+           if(fc === false){
+            alert("welcome Ms"+" "+n)
            }
-         }else if (g==="female"){
-            let fc = confirm("do you want to skip welcome")
-            if(fc === false){
-             alert("welcome Ms"+" "+n)
-            }
-         } else {let c = confirm("do you want to skip welcome")
-         if(c === false){
-          alert("welcome "+" "+n)
-         }
-        } 
+        } else {let c = confirm("do you want to skip welcome")
+        if(c === false){
+         alert("welcome "+" "+n)
+        }
+       } 
+       
     }
     
 

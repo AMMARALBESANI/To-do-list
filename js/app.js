67ce0bf3eca4b993c1b7175d028a43e14917ec98
AMMARALBESANI <Ammarmohammed018@gmail.com>
2023-04-05 06:37:25 +0300

    add js2

--- js/app.js
+++ js/app.js
@@ -7,20 +7,24 @@ let n = prompt("please enter your name")
 if (n == ""){
     prompt("please add yor name ")
 
-}else{
-    let age = prompt("please"+" "+n+" "+"enter your age")
+}
+
+    
+let age = prompt("please"+" "+n+" "+"enter your age")
+
     if (age <=0){
         prompt("please enter valid age")
-    }
+     }
 
-        let g = prompt("please"+" " +n+" "+"enter your gender")
-        if(g==="male"){
-          let mc = confirm("do you want to skip welcome")
+
+let g = prompt("please"+" " +n+" "+"enter your gender")
+  if(g==="male"){
+      let mc = confirm("do you want to skip welcome")
           if(mc === false){
            alert("welcome Mr"+" "+n)
           }
-        }else if (g==="female"){
-           let fc = confirm("do you want to skip welcome")
+  }else if (g==="female"){
+         let fc = confirm("do you want to skip welcome")
            if(fc === false){
             alert("welcome Ms"+" "+n)
            }
@@ -29,8 +33,28 @@ if (n == ""){
          alert("welcome "+" "+n)
         }
        } 
-       
-    }
+
+  let question =[prompt("Are you student"),prompt("Would you rate us"),prompt("Is our app useful")];
+  let answer =[];
+  function traverse (){
+     for(let i =0; i<question.length;i++){
+         if (question[i]=="yes"){
+        answer.push(question[i])
+         }else if(question[i]=="no") {
+        answer.push(question[i])
+         }else{
+            answer.push("invalid")
+         }
+  }
+  console.log(answer)
+}
+ traverse();
+
+ 
+   
+     
+  
+
  
 
 
